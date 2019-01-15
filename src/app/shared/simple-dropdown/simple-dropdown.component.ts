637306605995d630d66813cf12c6e7c3e44e8f23
Vadim Bochenev <vabochen@merann.ru>
2019-01-15 21:56:34 +0300

    last update

--- src/app/shared/simple-dropdown/simple-dropdown.component.ts
+++ src/app/shared/simple-dropdown/simple-dropdown.component.ts
@@ -3,7 +3,7 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-simple-dropdown',
   templateUrl: './simple-dropdown.component.html',
-  styleUrls: ['./simple-dropdown.component.css']
+  styleUrls: ['./simple-dropdown.component.scss']
 })
 export class SimpleDropdownComponent implements OnInit {
 
